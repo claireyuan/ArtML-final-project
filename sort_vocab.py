95c7604378cc4082f9f2ad05520b4bdc965cadf6
Claire Yuan <claireyuan97@gmail.com>
2019-04-17 13:13:38 -0400

    vocab

--- sort_vocab.py
+++ sort_vocab.py
@@ -1,5 +1,5 @@
-with open('lilypond_vocab.rtf', 'r') as f, open('lilypond_vocab_sorted.txt', 'w') as w:
+with open('lilypond_vocab.txt', 'r') as f, open('lilypond_vocab_sorted.txt', 'w') as w:
 	vocab = f.readlines()
-	vocab.sort(key=len)
+	vocab.sort(key=len, reverse=True)
 	for word in vocab:
-		w.write(word + '\n')
+		w.write(word)
