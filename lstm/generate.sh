5593a00df37432f8cf340f97fe6ca8d6a8d876e8
Claire Yuan <claireyuan97@gmail.com>
2019-04-19 15:00:42 -0400

    bug fixes

--- lstm/generate.sh
+++ lstm/generate.sh
@@ -3,7 +3,7 @@
 set -x
 
 python generate.py --cuda \
-	--data data \
+	--data data/clean \
 	--checkpoint ./model${1}.pt \
 	--outf generated${1}.txt \
 	--words 100000 # 2>&1 | tee -a train${1}.log
