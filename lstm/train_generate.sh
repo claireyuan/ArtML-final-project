10720fd06d4f5a4392d28e153a434e63d78675e5
claireyuan <ubuntu@ip-172-31-37-210.us-east-2.compute.internal>
2019-04-22 19:30:40 +0000

    script

--- lstm/train_generate.sh
+++ lstm/train_generate.sh
@@ -7,26 +7,30 @@ mkdir ${1}
 python main.py --cuda \
 	--data ../data/script_cleaned \
 	--model GRU \
-	--emsize 32 \
-	--nhid 128 \
+	--emsize 16 \
+	--nhid 256 \
 	--nlayers 2 \
 	--batch_size 64 \
 	--lr 10 \
 	--clip 0.25 \
-	--epochs 100 \
+	--bptt 200 \
+	--epochs 300 \
+	--checkpoint ./${1}/checkpoint.pt \
 	--save ./${1}/model.pt # 2>&1 | tee -a train${1}.log
 
 python main.py --cuda \
 	--reload ./${1}/checkpoint.pt \
 	--data ../data/manually_cleaned \
 	--model GRU \
-	--emsize 32 \
-	--nhid 128 \
+	--emsize 16 \
+	--nhid 256 \
 	--nlayers 2 \
 	--batch_size 64 \
 	--lr 1 \
 	--clip 0.25 \
-	--epochs 50 \
+	--bptt 200 \
+	--epochs 150 \
+	--checkpoint ./${1}/tuned_checkpoint.pt \
 	--save ./${1}/model_tuned.pt # 2>&1 | tee -a train${1}.log
 
 python generate.py --cuda \
