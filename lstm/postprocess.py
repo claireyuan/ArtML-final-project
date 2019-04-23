f5aa0a97173f5cb513627a18f762115bb63c2190
Claire Yuan <claireyuan97@gmail.com>
2019-04-23 12:05:07 -0400

    postprocessing script

--- lstm/postprocess.py
+++ lstm/postprocess.py
@@ -1,14 +1,18 @@
-"""
-Post-processing script removing excess white space and <eos> tags,
-and inserting new lines between tweets.
-"""
-
 import sys
 
-with open(sys.argv[1], 'r') as f, open(sys.argv[2], 'w') as out:
-	for tweet in f.read().split('<eos>'):
-		tweet_char = list(tweet)
-		if (tweet_char[0] is ' '):
-			out.write(''.join(list(tweet)[1::2]) + '\n')
-		else:
-			out.write(''.join(list(tweet)[::2]) + '\n')
+in_file = sys.argv[1]
+out_file = sys.argv[2]
+
+with open(in_file, 'r') as r, open(out_file, 'w') as w:
+	examples = r.read().split('<eos>')
+	for example in examples:
+		tokens = example.split()
+		cleaned = ' '
+		for token in tokens:
+			if token in list('abcdefgr'):
+				cleaned += (' ' + token)
+			elif token in ['is', 'es', 's', '\'', '.', '-']:
+				cleaned += (token)
+			else: 
+				cleaned += (' ' + token + ' ')
+		w.write(' '.join(cleaned.split()) + '\n\n')
